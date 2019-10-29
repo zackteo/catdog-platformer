f11ae266a658c1528ff63097034f3b880a6ae008
blacksiege <weikang97@gmail.com>
2019-10-29 20:49:18 +0800

    dog attack
    cat is now 'enemy' pls refer health inside it i guess im not too sure do not mess with the tags nothing happened there
    
    dog attacks in a circle yay

---
+++ Assets/plater_attack.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class plater_attack : MonoBehaviour
+{
+    private float timeBtwAttack = 2;
+    public float startTimeBtwAttack;
+    public int damage;
+
+    public Transform attackPos;
+    public LayerMask whatIsEnemies;
+    public float attackRange;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timeBtwAttack <= 0)
+        {
+            if (Input.GetKey(KeyCode.Space)) {
+                timeBtwAttack = startTimeBtwAttack;
+                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemies);
+                Debug.Log("Ipressedbuttonyes");
+                for (int i = 0; i < enemiesToDamage.Length; i++)
+                {
+                    enemiesToDamage[i].GetComponent<Enemy>().TakeDamage(damage);
+                    Debug.Log("Ihithimseeeee");
+                }
+            }
+        } else
+        {
+            timeBtwAttack -= Time.deltaTime;
+        }
+        
+    }
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPos.position, attackRange);
+    }
+}
