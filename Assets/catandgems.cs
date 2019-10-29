cdc1541091c45278cff0e134e7729ced3209293c
blacksiege <weikang97@gmail.com>
2019-10-29 22:16:23 +0800

    cat and gems
    theres a glitch somewhere that the cat could get two gems sometimes but other than that the cat can now collect gems yes yes

---
+++ Assets/catandgems.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class catandgems : MonoBehaviour
+{
+    private gamemastergems gm;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gm = GameObject.FindGameObjectWithTag("Gamemaster").GetComponent<gamemastergems>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Gem"))
+        {
+            Destroy(collision.gameObject);
+            gm.points = gm.points+ 1;
+            Debug.Log("i_can_has_gem");
+        }
+
+    }
+}
---
+++ Assets/gamemastergems.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gamemastergems : MonoBehaviour
+{
+    public int points;
+    public string pointsText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pointsText = ("Points" + points);
+    }
+}
