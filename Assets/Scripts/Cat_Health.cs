22b316930859b35a9670c15964771dc8844d6c68
Miles <victoriayong@gmail.com>
2019-11-07 17:08:51 +0800

    Health and health script

---
+++ Assets/Scripts/Cat_Health.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cat_Health : MonoBehaviour
+{
+    public int Health;
+    public int nHearts;
+
+    public Image[] hearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+
+    void Update()
+    {
+        if(Health > nHearts)
+        {
+            Health = nHearts;
+        }
+
+        for (int i = 0; i < hearts.Length; i++)
+        {
+            if(i < Health)
+            {
+                hearts[i].sprite = fullHeart;
+            }
+            else
+            {
+                hearts[i].sprite = emptyHeart;
+            }
+            if (i < nHearts)
+            {
+                hearts[i].enabled = true;
+            }
+            else
+            {
+                hearts[i].enabled = false;
+            }
+        }
+    }
+}
