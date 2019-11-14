c282fbd18e056d5a55c5a774efecf578b99ad7b7
Miles <victoriayong@gmail.com>
2019-11-14 18:43:35 +0800

    added hearts, edited code for ladder

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,6 +10,13 @@ public class PlayerMovement : MonoBehaviour {
     public string jumpButton = "Jump_P1";
     public string crouchButton = "Crouch_P1";
     public string horizontalCtrl = "Horizontal_P1";
+    public float distance;
+    public LayerMask whatIsLadder;
+    private bool climbing;
+    private float inputVertical;
+    private float inputHorizontal;
+    private Rigidbody2D rb;
+    
 
 	float horizontalMove = 0f;
 	bool jump = false;
@@ -40,5 +47,37 @@ public class PlayerMovement : MonoBehaviour {
 		// Move our character
 		controller.Move(horizontalMove * Time.fixedDeltaTime, crouch, jump);
 		jump = false;
+
+        // Climb Ladders
+        inputHorizontal = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(inputHorizontal * runSpeed, rb.velocity.y);
+
+        RaycastHit2D hitInfo = Physics2D.Raycast(transform.position, Vector2.up, distance, whatIsLadder);
+
+        if(hitInfo.collider != null)
+        {
+            if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                climbing = true;
+            }
+            else
+            {
+                if (Input.GetKeyDown(KeyCode.upArrow)) || Input.GetKeyDown(KeyCode.RightArrow))
+                {
+                    climbing = false;
+                }
+            }
+
+            if(climbing == true && hitInfo.collider !=null)
+            {
+                inputVertical = Input.GetAxisRaw("Vertical");
+                rb.velocity = new Vector2(rb.position.x, inputVertical * runSpeed);
+                rb.gravityScale = 0;
+            }
+            else
+            {
+                rb.gravityScale = 5;
+            }
+        }
 	}
 }
